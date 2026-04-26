9f1b1c9bde7adad277314b8b66c6b4fe98a5062c
Manish Kumar Yadav <162919290+maniishh@users.noreply.github.com>
2026-04-26 22:39:17 +0530

    Update comment.model.js

--- backend/src/models/comment.model.js
+++ backend/src/models/comment.model.js
@@ -1,26 +1,30 @@
 import mongoose, { Schema } from "mongoose";
 import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
 
-const commentSchema = new Schema({
- 
-    content:{
-        type:String,
-        required:true,
-    },
-    video:{
-        type:mongoose.Schema.Types.ObjectId,
-        ref:"Video"    
+const commentSchema = new Schema(
+    {
+        content: {
+            type: String,
+            required: true,
         },
-        owner:{
-            type:mongoose.Schema.Types.ObjectId,
-            ref:"User"
+        video: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Video"
+        },
+        owner: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User"
+        },
+      
+        parentComment: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Comment",
+            default: null
         }
-
-},
-{    timestamps:true
-}
+    },
+    { timestamps: true }
 )
 
 commentSchema.plugin(mongooseAggregatePaginate)
 
-export const Comment = mongoose.model("Comment",commentSchema)
+export const Comment = mongoose.model("Comment", commentSchema)
